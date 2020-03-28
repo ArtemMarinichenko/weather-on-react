f388008d65eeb6024f932b1cd26f2bd25b5f007f
Artem1989-dot <artemmarinichenko@ukr.net>
2020-03-28 13:20:21 +0300

    Updates

---
+++ precache-manifest.ff0b685b09d0c90a64048fa5b0e3e477.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0434261f35930ccf1c04b2707f656e9",
+    "url": "/weather-on-react/index.html"
+  },
+  {
+    "revision": "e6d46ab7900277fed1dd",
+    "url": "/weather-on-react/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "df879b5cd53fa38026be",
+    "url": "/weather-on-react/static/css/main.14cfe9ba.chunk.css"
+  },
+  {
+    "revision": "e6d46ab7900277fed1dd",
+    "url": "/weather-on-react/static/js/2.59d7c861.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-on-react/static/js/2.59d7c861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df879b5cd53fa38026be",
+    "url": "/weather-on-react/static/js/main.991cb68a.chunk.js"
+  },
+  {
+    "revision": "e76f5dd49c271736ec0b",
+    "url": "/weather-on-react/static/js/runtime-main.81ff4bb2.js"
+  }
+]);
